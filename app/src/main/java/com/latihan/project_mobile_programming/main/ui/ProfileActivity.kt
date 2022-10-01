ae825f6d88eb58adff50c17112176795a0932558
marellaaz <marellaazariaputri@gmail.com>
2022-10-01 15:07:28 +0700

    ini profile ges dari marella cantik

---
+++ app/src/main/java/com/latihan/project_mobile_programming/main/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.latihan.project_mobile_programming.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.latihan.project_mobile_programming.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
